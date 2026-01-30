c7a984bbaae916ed9e3e01257098e98a74a0a76f
Ochyy <ochydiza@gmail.com>
2026-01-31 05:54:25 +0700

    fix: disable static export

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // ⛔ PENTING: jangan static export
-  output: "standalone",
-
-  // ⛔ BLOK API dari static generation
-  experimental: {
-    appDir: true,
-  },
+  output: "standalone", // ⛔ ini yang MATIIN static export
 };
 
 module.exports = nextConfig;
